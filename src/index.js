82e11f6cf1975509c3df0ce3d65d73eb2bb6388a
João Gustavo <150349750+GustavoCarvalho00@users.noreply.github.com>
2023-11-09 11:45:25 -0300

    fimDeProjeto.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-let nome = "Joao Gustavo";
+let nome;
+let xp;
 
-console.log(nome);
+nome = prompt("Digite o nome de seu personagem ");
+xp = prompt("Digite o nivel de xp (valor deve estar entre 0 e 10001 ");
+
+switch (xp > -1) {
+    case xp <= 1000:
+        console.log("o heroi de nome " + nome + " está na liga de Ferro")
+        break
+
+    case (xp >= 1001 && xp <= 2000):
+        console.log("o heroi de nome " + nome + " está na liga de bronze")
+        break
+
+    case (xp >= 2001 && xp <= 5000):
+        console.log("o heroi de nome " + nome + " está na liga de Prata")
+        break
+
+    case (xp >= 6001 && xp <= 7000):
+        console.log("o heroi de nome " + nome + " está na liga de Ouro")
+        break
+
+    case (xp >= 7001 && xp <= 8000):
+        console.log("o heroi de nome " + nome + " está na liga de Platina")
+        break
+
+    case (xp >= 8001 && xp <= 9000):
+        console.log("o heroi de nome " + nome + " está na liga de Ascendente")
+        break
+
+    case (xp >= 9001 && xp <= 10000):
+        console.log("o heroi de nome " + nome + " está na liga de Imortal")
+        break
+
+    case xp >= 10001:
+        console.log("o heroi de nome " + nome + " está na liga Radiante")
+        break
+}
